5a050aa228c9d63a817ff6c67d19a5767261d85a
jtgrimaldi <137420639+jtgrimaldi@users.noreply.github.com>
2023-06-22 11:11:38 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My GitHub Page</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <h1>Welcome to my GitHub Page</h1>
+    <p>This is a basic GitHub Pages site.</p>
+    <div id="quiz"></div>
+    <button id="submit">Submit Answer</button>
+    <div id="results"></div>
+
+    <script src="script.js"></script>
+  </body>
+</html>
