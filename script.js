30276a1f1a2ed827c86a1024f2d98846d2f9d85b
jtgrimaldi <137420639+jtgrimaldi@users.noreply.github.com>
2023-06-23 16:36:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>My GitHub Page</title>
-    <link rel="stylesheet" href="styles.css">
-  </head>
-  <body>
-    <h1>Welcome to my GitHub Page</h1>
-    <p>This is a basic GitHub Pages site.</p>
-    <div id="quiz"></div>
-    <button id="submit">Submit Answer</button>
-    <div id="results"></div>
+let quiz = [
+  {
+    question: "Which of the following correctly uses the adjective ending for a masculine noun in the nominative case?",
+    options: [
+      "Der alte Mann geht zum Markt.",
+      "Der alten Mann geht zum Markt.",
+      "Der altem Mann geht zum Markt."
+    ],
+    answer: 0
+  },
+  {
+    question: "Which of the following correctly uses the adjective ending for a feminine noun in the accusative case?",
+    options: [
+      "Ich sehe die alte Frau.",
+      "Ich sehe die alten Frau.",
+      "Ich sehe die altem Frau."
+    ],
+    answer: 0
+  },
+  // Add more questions as needed
+];
 
-    <script src="script.js"></script>
-  </body>
-</html>
+let currentQuestion = 0;
+
+function showQuestion() {
+  let question = quiz[currentQuestion];
+  let questionDiv = document.getElementById('quiz');
+  questionDiv.innerHTML = '<p>' + question.question + '</p>';
+
+  for(let i=0; i<question.options.length; i++) {
+    questionDiv.innerHTML += '<button onclick="submitAnswer(' + i + ')">' + question.options[i] + '</button>';
+  }
+}
+
+function submitAnswer(option) {
+  let question = quiz[currentQuestion];
+  let resultsDiv = document.getElementById('results');
+  
+  if(option === question.answer) {
+    resultsDiv.innerHTML = "Correct!";
+  } else {
+    resultsDiv.innerHTML = "Sorry, that's incorrect.";
+  }
+  
+  currentQuestion++;
+  
+  if(currentQuestion < quiz.length) {
+    showQuestion();
+  } else {
+    // Quiz is over
+    resultsDiv.innerHTML += "<p>Quiz complete!</p>";
+  }
+}
+
+showQuestion();
