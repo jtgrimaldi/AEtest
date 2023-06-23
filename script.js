f0e6282b80fd7296a72f48d65d0460e8bf7e1314
jtgrimaldi <137420639+jtgrimaldi@users.noreply.github.com>
2023-06-23 16:56:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 let quiz = [
   {
-    question: "Which of the following correctly uses the adjective ending for a masculine noun in the nominative case?",
-    options: [
-      "Der alte Mann geht zum Markt.",
-      "Der alten Mann geht zum Markt.",
-      "Der altem Mann geht zum Markt."
-    ],
-    answer: 0
+    question: "Ich sehe die alt____ Frau. Wähle das korrekte Adjektivende.",
+    options: ["e", "er", "em", "en"],
+    answer: 0 // "e" is the correct adjective ending for a feminine noun in the accusative case.
   },
   {
-    question: "Which of the following correctly uses the adjective ending for a feminine noun in the accusative case?",
-    options: [
-      "Ich sehe die alte Frau.",
-      "Ich sehe die alten Frau.",
-      "Ich sehe die altem Frau."
-    ],
-    answer: 0
+    question: "Der alt____ Mann geht zum Markt. Wähle das korrekte Adjektivende.",
+    options: ["e", "er", "em", "en"],
+    answer: 3 // "en" is the correct adjective ending for a masculine noun in the nominative case.
   },
   // Add more questions as needed
 ];
@@ -23,33 +15,35 @@ let quiz = [
 let currentQuestion = 0;
 
 function showQuestion() {
-  let question = quiz[currentQuestion];
-  let questionDiv = document.getElementById('quiz');
-  questionDiv.innerHTML = '<p>' + question.question + '</p>';
-
-  for(let i=0; i<question.options.length; i++) {
-    questionDiv.innerHTML += '<button onclick="submitAnswer(' + i + ')">' + question.options[i] + '</button>';
+  let q = quiz[currentQuestion];
+  let quizDiv = document.getElementById('quiz');
+  quizDiv.innerHTML = '';
+  let question = document.createElement('p');
+  question.innerText = q.question;
+  quizDiv.appendChild(question);
+  for (let i = 0; i < q.options.length; i++) {
+    let option = document.createElement('button');
+    option.innerText = q.options[i];
+    option.addEventListener('click', function() { checkAnswer(i); });
+    quizDiv.appendChild(option);
   }
 }
 
-function submitAnswer(option) {
-  let question = quiz[currentQuestion];
+function checkAnswer(userAnswer) {
+  let q = quiz[currentQuestion];
   let resultsDiv = document.getElementById('results');
-  
-  if(option === question.answer) {
-    resultsDiv.innerHTML = "Correct!";
+  if (userAnswer === q.answer) {
+    resultsDiv.innerText = 'Richtig!';
   } else {
-    resultsDiv.innerHTML = "Sorry, that's incorrect.";
+    resultsDiv.innerText = 'Leider falsch. Versuche es noch einmal.';
   }
-  
   currentQuestion++;
-  
-  if(currentQuestion < quiz.length) {
+  if (currentQuestion < quiz.length) {
     showQuestion();
   } else {
-    // Quiz is over
-    resultsDiv.innerHTML += "<p>Quiz complete!</p>";
+    resultsDiv.innerText += '\n Du hast das Quiz abgeschlossen!';
   }
 }
 
+// Start the quiz by showing the first question
 showQuestion();
